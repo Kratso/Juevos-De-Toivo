d4a7757fee203febc141f34f31846780034e7a62
Mateo García Neves <kratsothelsamar@gmail.com>
2020-01-02 17:07:58 +0100

    Add files via upload

---
+++ build/precache-manifest.29787ee2f6e8dfac611d11b27253fbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724f08b2c3e06511ac6b6df126a2e626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b5bcba43b8300a4e2a",
+    "url": "/static/css/main.7fec8138.chunk.css"
+  },
+  {
+    "revision": "41b1c3d4fecf4cea3d39",
+    "url": "/static/js/2.69ba327b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.69ba327b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3b5bcba43b8300a4e2a",
+    "url": "/static/js/main.5318a3d7.chunk.js"
+  },
+  {
+    "revision": "6026d070d4085badd04f",
+    "url": "/static/js/runtime-main.cd066a53.js"
+  },
+  {
+    "revision": "c2a32ef2fd693f3f58bb2733fc8305e6",
+    "url": "/static/media/logo.c2a32ef2.svg"
+  }
+]);
